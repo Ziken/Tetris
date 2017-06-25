2235f489af39e45a3b4466f2b27e81d52e9a606a
Ziken <patrykkazmierczak97@gmail.com>
2017-06-25 20:53:08 +0200

    Add function 'endGame' which ends game very beautiful ;), add variable which allows block keys

--- js/CanvasScreen.js
+++ js/CanvasScreen.js
@@ -43,7 +43,24 @@ var CanvasScreen = function ( canvasElem ) {
 
             currentX = 0;
             currentY += SIZE;
-
         });
+    }
+    /**
+        public function, draw text on screen, at center.
+        @param {string} text what write
+        @param {string} colorFill hex color of filled text
+        @param {string} colorStroke hex color of border of text
+        @param {integer} fontSize size of font in px
+    */
+    this.drawText = function ( text, colorFill, colorStroke, fontSize ) {
+        ctx.beginPath();
+        ctx.font = "bold " + fontSize + "px Comic Sans MS";
+        ctx.fillStyle = colorFill;
+        ctx.strokeStyle = colorStroke;
+        ctx.lineWidth = 3;
+        ctx.textAlign = "center";
+        ctx.fillText(text, canvasElem.width/2, canvasElem.height/2);
+        ctx.strokeText(text, canvasElem.width/2, canvasElem.height/2);
+
     }
 }
